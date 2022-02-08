488ce5673ee366665ec9e0c3179320e934cf133f
Garrett-In-Development <critesg@spu.edu>
2022-02-07 20:55:24 -0800

    Changed Smart Gaps

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappih    = 16;	/* inner horizontal gaps */
 static const unsigned int gappiv    = 16;	/* inner vertical gaps */
 static const unsigned int gappoh    = 16;	/* outer horizontal gaps */
 static const unsigned int gappov    = 16;	/* outer vertical gaps */
-static const int smartgaps          = 0;	/* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;	/* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14", "JoyPixels:antialias=true:size=10" };
